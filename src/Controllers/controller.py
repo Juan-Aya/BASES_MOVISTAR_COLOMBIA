b776350654043f854806fbf8a753114f1853dd9e
CesarAlmeciga00 <cesaralmeciga12@gmail.com>
2024-01-04 08:12:25 -0500

    cargue del proyecto  al repositorio

--- src/Controllers/controller.py
+++ src/Controllers/controller.py
@@ -14,7 +14,7 @@ from Imports import *
 #-- Funciónes de tratamiento y cargue la informacion a las tablas: toSqlTxt,toSqlExcel
 #-- Funciónes de tratamiento de datos en columnas: convertir_fecha,insertar_raya_al_piso 
 
-with open("/root/PROCESOS/BASESMOVISTAR/src/log.yml") as f:
+with open("/root/PROCESOS/BASESCLAROCOBRANZAPRUEBAS/src/log.yml") as f:
     cnf = yaml.safe_load(f)
     logging.config.dictConfig(cnf)
 
@@ -42,7 +42,7 @@ def Read_files_path(path_,nombre_tabla,nombre_archivo):
         print("error")
         logging.getLogger("user").exception(e)
         raise 
-    with open(f"//root//PROCESOS//BASESMOVISTAR//src//LoadedFiles//{nombre_tabla}.log", "r") as f:
+    with open(f"//root//PROCESOS//BASESCLAROCOBRANZAPRUEBAS//src//LoadedFiles//{nombre_tabla}.log", "r") as f:
         loaded_files = f.read().splitlines()  # todo el listado 
         # Obtener la lista de archivos con fechas de modificación
         archivos_con_fecha = [f"{datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(path_, archivo))).strftime('%Y-%m-%d %H:%M:%S')} - {archivo}" for archivo in archivos_coincidentes]
@@ -71,34 +71,38 @@ def insertar_raya_al_piso(cadena): #Funcion que inserta raya al piso cuando el e
             return cadena
 
 def convertir_fecha(fecha):
-    try:
-        fecha=pd.to_datetime(fecha, format="%Y-%m-%d %I:%M:%S")
-    except:
-        try:
-            fecha= pd.to_datetime(fecha, format="%Y-%m-%d")
-        except:
-            try:
-                fecha=pd.to_datetime(fecha, format="%Y/%m/%d")
-            except:
-                try:
-                    fecha=pd.to_datetime(fecha, format="%d/%m/%Y")
-                except:
-                    try: 
-                        fecha=pd.to_datetime(fecha, format="%m-%d-%y")
-                    except:
-                        try:
-                            if len(str(fecha)) <=5 :
-                                x = datetime.datetime(1900, 1, 1)
-                                fecha = x + datetime.timedelta(days=(int(fecha) - 2))
-                        except:
-                            pass
+    formatos = ["%Y-%m-%d %I:%M:%S", "%d/%m/%Y %I:%M:%S", "%Y-%m-%d", "%Y/%m/%d", "%d/%m/%Y", "%m-%d-%y", "%d/%m/%Y:%H:%M:%S", "%d-%m-%Y %H:%M:%S", "%d/%m/%Y 0:00:00", "%d/%m/%Y 00:00:00", "%d/%m/%Y %H:%M:%S","%Y-%m-%d %H:%M:%S"]
 
-    return str(fecha)
+    for formato in formatos:
+        try:
+            fecha = pd.to_datetime(fecha, format=formato)
+            return str(fecha)
+        except Exception as e:
+            #print("ERROR: con la fecha: ",e)
+            pass
+    
+        # Si no coincide con ninguno de los formatos, intenta manejar el caso especial de fechas cortas
+        try:
+            if len(str(fecha)) <= 5:
+                x = datetime(1900, 1, 1)
+                fecha = x + pd.to_timedelta(int(fecha) - 2, unit='D')
+                return str(fecha)
+        except ValueError:
+                pass
+    
+                # Si no puede convertir la fecha, devuelve un mensaje indicando el problema
+                return "No se pudo convertir la fecha"
             
-def toSqlTxt(path,nombre_tabla,file_, dic_fechas, dic_formatos, separador):
+def toSqlTxt(path,nombre_tabla,file_, dic_fechas, dic_formatos, separador, columnas_tabla):
     logging.getLogger("user").info(f"cargando archivo de texto")
     logging.info('Lectura archivo')
-    df = dd.read_csv(path+"/"+file_[22:],sep = separador,dtype=str, index_col=False)
+    print(path)
+    print(file_)
+    if len(columnas_tabla)==0:
+        df = dd.read_csv(path+"/"+file_[22:],sep = separador,dtype=str, index_col=False)
+    else:
+        df = dd.read_csv(path+"/"+file_[22:],sep = separador,dtype=str, index_col=False, names=columnas_tabla, encoding='latin-1')
+    print(df)
     fecha= datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(path, file_[22:])))
     tabla_reemplazo = str.maketrans({"á":"a","é":"e","í":"i","ó":"o","ú":"u","ñ":"n","Á":"A","É":"E","Í":"I","Ó":"O","Ú":"U","Ñ":"N"})
     df.columns = df.columns.str.strip()
@@ -112,7 +116,7 @@ def toSqlTxt(path,nombre_tabla,file_, dic_fechas, dic_formatos, separador):
         if fecha_mod in df.columns:
             df[fecha_mod] = df[fecha_mod].apply(convertir_fecha, meta=(f"{fecha_mod}", "str"))
            
-    df['FILE_DATE'] = fecha
+    df['FILE_DATE'] = fecha                                     
     df['FILE_NAME'] = file_[22:]
     df['FILE_YEAR'] = df['FILE_DATE'].dt.year
     df['FILE_MONTH']  = df['FILE_DATE'].dt.month
@@ -140,7 +144,7 @@ def toSqlTxt(path,nombre_tabla,file_, dic_fechas, dic_formatos, separador):
         tmp.create(bind = engine)
         logging.getLogger("user").info('Creacion Tabla temporal')
         # Cargue del DataFrame de Dask en la base de datos MySQL.
-        df.to_sql(tmp.name, connection, if_exists='append',index=False,parallel=True)
+        df.to_sql(tmp.name, cdn_connection, if_exists='append',index=False,parallel=True)
         connection.execute(text(f"INSERT IGNORE INTO {tabla.name} SELECT * FROM {tmp.name};"))
         tmp.drop(bind = engine,checkfirst=True)
         logging.getLogger("user").info('Insercion Tabla destino DW')
@@ -149,24 +153,27 @@ def toSqlTxt(path,nombre_tabla,file_, dic_fechas, dic_formatos, separador):
         logging.getLogger("user").exception(e)
         raise
 
-def toSqlExcel(path,nombre_tabla,file_, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion):
+def toSqlExcel(path,nombre_tabla,file_, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion,columnas_tabla , columnas_sin_espacio):
     try:
         logging.info('Lectura archivo')
         excel_file = pd.ExcelFile(path+"/"+file_[22:])
-        hojas_documento = excel_file.sheet_names
+        hojas_documento = dic_hojas
         fechaHoraActual=datetime.datetime.now()
-        for sheet_1 in hojas_documento:
-            Xlsx2csv(path+"/"+file_[22:], outputencoding="utf-8").convert(os.path.join("TemporalFiles",f"{sheet_1}.csv"), sheetname=sheet_1)
-            logging.info(f"se creó el archivo {sheet_1}.csv")
-        
-        archivos_hojas = os.listdir(os.path.join("TemporalFiles"))
-        for sheet in archivos_hojas:
+
+        for sheet in hojas_documento:
+            if sheet == "None":
+                sheet=0
             try:
-                df = dd.read_csv(os.path.join("TemporalFiles", sheet), sep=",", dtype=str)
-                if "B2B.csv" in hojas_documento and asignacion==1:
-                    df["CAMPAÑA"]="B2B"
-                elif asignacion==1:
-                    df["CAMPAÑA"]="B2C"
+                if len(columnas_tabla)==0:
+                    df = pd.read_excel(path+"/"+file_[22:], sheet_name = sheet, dtype=str)
+                else:
+                    df = pd.read_excel(path+"/"+file_[22:],sheet_name = sheet, dtype=str, names=columnas_tabla)
+                if sheet == "CUENTAS" and "Exclusion_Gestion_Cuentas" in file_:
+                    df1= df[['CUENTA', 'FECHA', 'MES', 'SDS']]
+                    df2 = df[['CUENTA.1', 'FECHA.1', 'MES.1']]
+                    print(df2)
+                    df2.rename(columns={'CUENTA.1':'CUENTA','FECHA.1':'FECHA',  'MES.1': 'MES'}, inplace=True)
+                    df= pd.concat([df1, df2], axis=0)
                 fecha= datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(path, file_[22:])))
                 tabla_reemplazo = str.maketrans({"á":"a","é":"e","í":"i","ó":"o","ú":"u","ñ":"n","Á":"A","É":"E","Í":"I","Ó":"O","Ú":"U","Ñ":"N"})
                 df.columns = df.columns.str.strip()
@@ -177,23 +184,28 @@ def toSqlExcel(path,nombre_tabla,file_, dic_fechas,dic_formatos,dic_hojas,separa
                 df.columns = df.columns.str.upper()
                 for fecha_mod in dic_fechas:
                     if fecha_mod in df.columns:
-                        df[fecha_mod]=df[fecha_mod].apply(convertir_fecha, meta=(f"{fecha_mod}", "str"))
+                        df[fecha_mod]=df[fecha_mod].apply(convertir_fecha)
+                df= df.dropna(axis=1, thresh=2)
+                df= df.dropna(axis=0, how='all')
                 df['FILE_DATE'] = fecha
                 df['FILE_NAME'] = file_[22:]
                 df['FILE_YEAR'] = df['FILE_DATE'].dt.year
                 df['FILE_MONTH']  = df['FILE_DATE'].dt.month
-                df['FECHA_CARGUE'] = fechaHoraActual
-                if cargue_tabla == 1:
-                    df['HOJA_DATA'] = sheet[:-4]
+                print(df)
+                if cargue_tabla == 1 and len(hojas_documento)>1:
+                    df['HOJA_DATA'] = sheet
                     nombre_tabla1 = f"{nombre_tabla}"
+                elif cargue_tabla == 0 and len(hojas_documento)>1:
+                        nombre_tabla1= f"{nombre_tabla}_{sheet.lower()}"
                 else:
-                    if len(archivos_hojas)>=1:
-                        nombre_tabla1= f"{nombre_tabla}_{sheet}"
+                    nombre_tabla1 = f"{nombre_tabla}"
                 for clave in dic_formatos:
-                    valor = dic_formatos[clave]
-                    if valor[0] in df.columns:
-                        df[valor[0]] = df[valor[0]].str.replace(",", ".", regex=True)
-                        df[valor[0]] = df[valor[0]].str.replace("[^0-9-.]", "", regex=True)
+                    if clave in df.columns:
+                        df[clave] = df[clave].str.replace(",", ".", regex=True)
+                        df[clave] = df[clave].str.replace("[^0-9-.]", "", regex=True)
+                for clave in columnas_sin_espacio:
+                    if clave in df.columns:
+                        df[clave] = df[clave].str.replace("[^a-zA-Z0-9-@.,+]", "", regex=True)
                 connection,cdn_connection,engine,bbdd_or = mysql_connection()
                 logging.getLogger("user").info('Creacion Tabla temporal')
                 tabla = Table(f"tb_asignacion_{nombre_tabla1}", MetaData(), autoload_with = engine)
@@ -209,10 +221,8 @@ def toSqlExcel(path,nombre_tabla,file_, dic_fechas,dic_formatos,dic_hojas,separa
                 tmp = Table(f"{tabla.name}_tmp", MetaData(), *columnas_nuevas)
                 tmp.drop(bind = engine,checkfirst=True)
                 tmp.create(bind = engine)
-                dask.config.set(scheduler="processes")
-                df.repartition(npartitions=10)
                 # Cargue del DataFrame de Dask en la base de datos MySQL.
-                df.to_sql(tmp.name, cdn_connection, if_exists='append',index=False,parallel=True)
+                df.to_sql(tmp.name, cdn_connection, if_exists='append',index=False)
                 connection.execute(text(f"INSERT IGNORE INTO {tabla.name} SELECT * FROM {tmp.name};"))
                 tmp.drop(bind = engine,checkfirst=True)
                 logging.getLogger("user").info('Insercion Tabla destino DW')
@@ -221,40 +231,40 @@ def toSqlExcel(path,nombre_tabla,file_, dic_fechas,dic_formatos,dic_hojas,separa
                 logging.getLogger("user").exception(e)
                 raise
     except Exception as e:
-        print(f"ERROR {e}")
         logging.getLogger("user").exception(e)
         raise
-    finally:
-        for file in os.listdir(os.path.join("TemporalFiles")):
-            os.remove(os.path.join("TemporalFiles", file))
 
-def toSqlZip(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, nombre_archivo, cargue_tabla, asignacion, logs):
+def toSqlZip(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, nombre_archivo, cargue_tabla, asignacion, logs, columnas_sin_espacio,columnas_tabla):
+    print("Entrando a esta función...")
     try:
         archivo_zip = path+"/"+file[22:]
         logging.getLogger("user").info(f"El archivo a cargar es {archivo_zip}")
+        print("Se va a Descomprimir: ",archivo_zip)
         with zipfile.ZipFile(archivo_zip, 'r') as zip_ref:
             # Extrae todo el contenido en el directorio de destino
-            zip_ref.extractall(r"/root/PROCESOS/BASESMOVISTAR/src/ZIP/")
+    
+            zip_ref.extractall(r"/root/PROCESOS/BASESCLAROCOBRANZAPRUEBAS/src/ZIP/")
+
         file_to_load = Read_files_path(os.path.join("ZIP"),nombre_tabla,nombre_archivo)
+        print("lista de archivos: ",file_to_load)
         for file_1 in file_to_load:
             nombre, extension = os.path.splitext(file_1)
             if extension in [".txt", ".csv"]:
                 logging.getLogger("user").info(f"txt del zip")
-                toSqlTxt(path,nombre_tabla,file, dic_fechas,dic_formatos,separador)
+                toSqlTxt(os.path.join(os.getcwd(),"ZIP"),nombre_tabla,file_1, dic_fechas,dic_formatos,separador,columnas_tabla)
             elif extension in [".xlsx"]:
                 logging.getLogger("user").info(f"excel del zip")
-                toSqlExcel(os.path.join("ZIP"),nombre_tabla,file_1, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion)
+                toSqlExcel(os.path.join("ZIP"),nombre_tabla,file_1, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion, columnas_sin_espacio)
     except:
         raise
     finally:
         for file in os.listdir(os.path.join("ZIP")):
-            os.remove(os.path.join("ZIP", file))
-
+            os.remove(os.path.join("ZIP", file))         
 # Función terciaria del archivo
-def check_and_add(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion, nombre_archivo):
+def check_and_add(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion, nombre_archivo, columnas_tabla, columnas_sin_espacio):
     logging.getLogger("user").info(f" archivo a cargar{file}")
     ini = time.time() # timepo de inicio del cargue 
-    LOADED_FILES = f"//root//PROCESOS//BASESMOVISTAR//src//LoadedFiles//{nombre_tabla}.log" # ruta del log
+    LOADED_FILES = f"//root//PROCESOS//BASESCLAROCOBRANZAPRUEBAS//src//LoadedFiles//{nombre_tabla}.log" # ruta del log
 
     with open(LOADED_FILES, "r") as f: # abrir el archivo del log
         logs     = f.read().splitlines()
@@ -270,13 +280,13 @@ def check_and_add(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,sepa
         # validacion de extenciones para saber que función usar 
         if extension in [".txt", ".csv"]:
             logging.getLogger("user").info(f"el archivo es un txt")
-            toSqlTxt(path,nombre_tabla,file, dic_fechas,dic_formatos,separador)
-        elif extension in [".xlsx"]:
+            toSqlTxt(path,nombre_tabla,file, dic_fechas,dic_formatos,separador, columnas_tabla)
+        elif extension in [".xlsx", ".XLSX" ]:
             logging.getLogger("user").info(f"el archivo es un excel")
-            toSqlExcel(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion)
+            toSqlExcel(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion, columnas_tabla, columnas_sin_espacio)
         elif extension in [".zip", ".ZIP"]:
             logging.getLogger("user").info(f"el archivo es un zip")
-            toSqlZip(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, nombre_archivo, cargue_tabla,asignacion, logs)
+            toSqlZip(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, nombre_archivo, cargue_tabla,asignacion, logs, columnas_sin_espacio,columnas_tabla)
         send(f"Se acaba de cargar la base {file[22:]} ")
         logging.getLogger("user").info(f"Base '{file[22:]}' del {file[:20]} recien cargada [ToSQL]")
 
@@ -289,9 +299,8 @@ def check_and_add(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,sepa
         fin = time.time()
         print(f"TOTAL TIEMPO EJECUCION {fin-inicio}")
         logging.getLogger("user").info(f"Tiempo total de ejecucion: {fin-ini} de {file}")
-
 # Función primaria del archivo 
-def scan_folder(path,nombre_tabla,nombre_archivo,dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion):
+def scan_folder(path,nombre_tabla,nombre_archivo,dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion,columnas_tabla, columnas_sin_espacio):
     print(f"en Scan {nombre_tabla} -- {nombre_archivo}-- {path}")
     try:
         if os.path.exists(path): # validar si la ruta existe
@@ -300,11 +309,12 @@ def scan_folder(path,nombre_tabla,nombre_archivo,dic_fechas,dic_formatos,dic_hoj
             for file in file_to_load: # interar los archivos para el cargue
                 print("cargando",file)
                 # validacion y extandarización para el cargue de los archivos 
-                check_and_add(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion, nombre_archivo )   
+                check_and_add(path,nombre_tabla,file, dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion, nombre_archivo, columnas_tabla, columnas_sin_espacio )   
         else:
             if hora == '08:00':
                 # mesaje en caso de que no se encuentra la ruta deseada
                 send(f"No ha habido cargue de la base {nombre_tabla} del {anio}-{mes}-{dia}") 
+            logging.getLogger("user").info(f"no existe la ruta")
          
     except Exception as e:
         print(e)
--- src/main.py
+++ src/main.py
@@ -20,17 +20,15 @@ for i in range(len(list(archivos))):
     nombre_tabla   = coleccion[key1]["varibles"][1].strip("'")
     path_0         = coleccion[key1]["varibles"][2].strip("'")
     separador = coleccion[key1]["opcion_path"][1].strip("'")
-    limpieza = coleccion[key1]["opcion_path"][2].strip("'")
     cargue_tabla = coleccion[key1]["opcion_cargue_tabla"] 
     asignacion = coleccion[key1]["asignacion"]
-    
     # Selección de tipo de ruta a leer 
     if  coleccion[key1]["opcion_path"][0].strip("'") == "1":
-        path = f"{path_0}\{anio}\{mes}\{dia}"
+        path = f"{path_0}//2023//11//{dia}"
     elif coleccion[key1]["opcion_path"][0].strip("'") == "2":
-        path = f"{path_0}\{anio}\{mes}"
+        path = f"{path_0}//2023//11"
     elif coleccion[key1]["opcion_path"][0].strip("'") == "3":
-        path = f"{path_0}/{anio}"
+        path = f"{path_0}/2023"
     elif coleccion[key1]["opcion_path"][0].strip("'") == "4":
         path = f"{path_0}"
         
@@ -38,6 +36,9 @@ for i in range(len(list(archivos))):
     dic_fechas = coleccion[key1]["fechas"]
     dic_formatos = coleccion[key1]["formatos"]
     dic_hojas = coleccion[key1]["sheets"]
+    columnas_tabla = coleccion[key1]["columnas"]
+    columnas_sin_espacio =coleccion[key1]["columnas_sin_espacio"]
     if __name__ == '__main__':
         # Ejecucion de función primaria 
-        scan_folder(path,nombre_tabla,nombre_archivo,dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion)
+        scan_folder(path,nombre_tabla,nombre_archivo,dic_fechas,dic_formatos,dic_hojas,separador, cargue_tabla, asignacion, columnas_tabla, columnas_sin_espacio)
+
